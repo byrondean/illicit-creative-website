a9802630c86f791b8ebb97fa7422bed535842b0b
Ivan Grizelj <ivagri@stype.local>
2024-10-23 14:25:23 +0200

    Initial commit of Illicit Creative website

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Smooth scrolling effect when clicking on navigation buttons
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
