87ee34c80a0d49ed1a1fbc2dc1f84802714e3ac6
byrondean <drmz.rec@gmail.com>
2024-10-24 15:03:26 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,8 +2,13 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        const targetElement = document.querySelector(this.getAttribute('href'));
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start' // Ensures the section snaps to the top
+            });
+        }
     });
 });
